f7dabf95040ab6623e1eff74126bbc02381801e8
shahjee005 <you@example.com>
2020-06-15 17:14:28 -0600

    basic functions

--- numbers.py
+++ numbers.py
@@ -11,7 +11,27 @@ print(my_taxes)
 print (a) #comment 
 
 #Strings 
+'Hello World '
+"I'm a computer"
+mystring= 'Hello World!'
+print(mystring[0]) #H
+print(mystring[2:]) # llo World!
+print(mystring[:3]) # Hel
+print(mystring[0:5])# Hello
+print(mystring[::]) # Hello World!
+print(mystring[::2])# HloWrd
+print(mystring[::5])# HloWrd
 
+
+
+mystring ='Hello World'
+# x =mystring.upper() # upper case 
+x=mystring.capitalize()
+x= mystring.split('l')
+print(x)
+#print formating
+A= 'Item one :{x} Item Two : {y}'.format(x='apples', y='oranges')
+print (A)
 
 #Basics
 
